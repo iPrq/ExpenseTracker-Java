23c324637e6959144a73c3ca0b00e6b89aac4c8a
Neeraj Ciju <danciju@gmail.com>
2024-12-12 15:08:50 +0530

    Added functionality to "type" using ComboBox

--- src/main/java/main/expensemanager/ExpenseAdderController.java
+++ src/main/java/main/expensemanager/ExpenseAdderController.java
@@ -1,17 +1,21 @@
 package main.expensemanager;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class ExpenseAdderController  {
+public class ExpenseAdderController implements Initializable {
 
     @FXML
     private TextField amounttext;
@@ -19,17 +23,30 @@ public class ExpenseAdderController  {
     @FXML
     private TextField datetxt;
     DataBase dataBase;
+    @FXML
+    private ComboBox comboboxtype;
 
     @FXML
     private TextField expensetxt;
     private static Stage stage;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        comboboxtype.setItems(FXCollections.observableArrayList(
+                                                    "Leisure",
+                                                                "Entertainment",
+                                                                "Work",
+                                                                "Necessities",
+                                                                 "Travel", "Others"
+        ));
+    }
     @FXML
     void addbtnprsd() throws SQLException {
         dataBase = new DataBase();
         String expenses = expensetxt.getText();
         double amount = Double.parseDouble(amounttext.getText());
         String date = datetxt.getText();
-        String type = "type";
+        String type = comboboxtype.getValue() == null ? "Others": (String) comboboxtype.getValue();
         Expense expense = new Expense(expenses,amount,date,type);
         MainAppController.getInstance().addExpensePane(expense);
         dataBase.addData(expense);
